f20d94cb0b57bc4cf8fd6dfcf938561d9d7cce51
Liberontissauri <jp.liberon@gmail.com>
2025-02-18 17:43:12 +0000

    minimal nvim config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+	opts = {
+		ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { 'ruby' },
+		},
+		indent = { enable = true, disable = { 'ruby' } },
+	},
+}
